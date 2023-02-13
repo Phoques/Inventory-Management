5f602d54951bcbb16f4654fa1a126cbb1371ddae
Phoques <dylan.munro6@studytafensw.edu.au>
2023-02-13 16:44:22 +1100

    test

---
+++ Assets/Scripts/TimerSO.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Timer", menuName = "Default Timer")]
+public class TimerSO : ScriptableObject
+{
+    public float time;
+}
